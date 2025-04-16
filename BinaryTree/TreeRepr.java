37ea6196e93b33fac408ea2519f60b53fc8b69bc
Abhishek Pandey <abhishek.pandey.2k05@gmail.com>
2025-04-17 00:39:05 +0530

    updation

--- BinaryTree/TreeRepr.java
+++ BinaryTree/TreeRepr.java
@@ -1,43 +1,27 @@
-class Node {
-    // Stores the value
-    // of the node
-    int data;       
-    // Reference to the
-    // left child node
-    Node left; 
-    // Reference to the
-    // right child node
-    Node right;
 
-    // Constructor to initialize
-    // a node with a given key
+// This code is for Tree Construction in java .....
+
+class Node {
+    int data;        // Stores the value of the node
+    Node left;      // Reference to the left child node
+    Node right;     // Reference to the right child node
+
+
+    // Constructor to initialize  a node with a given key
     public Node(int key) {
-        // Assigns the provided key to
-        // the data field of the node
-        data = key;     
-        // Initializes left child 
-        // reference as null
-        left = null;    
-        // Initializes right child
-        // reference as null
-        right = null;   
+        data = key;      // Assigns the provided key to  the data field of the node
+        left = null;     // Initializes left child reference as null
+        right = null;    // Initializes right child reference as null
     }
 }
 
 public class TreeRepr {
     public static void main(String[] args) {
-         // Creates the root node
-         // with a key value of 1
-        Node root = new Node(1);       
-        // Creates a left child node for
-        // the root with a key value of 2
-        root.left = new Node(2);       
-         // Creates a right child node for
-         // the root with a key value of 3
-        root.right = new Node(3);      
-         // Creates a left child node for the right 
-         // child of the root with a key value of 5
-        root.right.left = new Node(5); 
+         
+        Node root = new Node(1);                                // Creates the root node with a key value of 1
+        root.left = new Node(2);                                // Creates a left child node for the root with a key value of 2
+        root.right = new Node(3);                               // Creates a right child node for the root with a key value of 3
+        root.right.left = new Node(5);                          // Creates a left child node for the right child of the root with a key value of 5
     }
 }
 
