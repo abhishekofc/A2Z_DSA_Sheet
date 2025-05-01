dab591aa3286d6acb3207d3d394983b9d556f6c8
Abhishek Pandey <abhishek.pandey.2k05@gmail.com>
2025-05-01 17:13:50 +0530

    .

--- Arrays/LargestElement.java
+++ Arrays/LargestElement.java
@@ -6,5 +6,15 @@ class Solution {
     }
 }
 
-// TIme Complexity : (nlog n )
-// Space Complexity :O(1)
+// TIme Complexity : (nlog n ) && Space Complexity :O(1) this method is neither a brute force not optimal..
+// The optimal approch would be a linear search ...
+
+/*
+  import java.util.*;
+class Solution {
+    public int largestElement(List<Integer> nums) {
+        Collections.sort(nums);
+        return nums.get[nums.size()-1];
+    }
+}
+ */
